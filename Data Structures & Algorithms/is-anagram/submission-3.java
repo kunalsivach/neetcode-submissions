6e70fbfb182927ce8293cac0a68fdbfb785fae2f
Kunal Sivach <106823798+kunalsivach@users.noreply.github.com>
2026-04-22 16:16:38 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,68 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //brut force
+
+        //we can create a hashmap, key = character
+        // value = number of times it appears
+
+        //then compare the 2 hashmap, if they are equal, then its a anagram
+       
+
+       //real brut force
+       HashMap<Character, Integer> shm = new HashMap<>();
+              HashMap<Character, Integer> thm = new HashMap<>();
+
+
+       char[] sArray = s.toCharArray();
+       char[] tArray = t.toCharArray();
+       if(sArray.length != tArray.length) return false;
+        // int pos;
+        // int pos1;
+    //    for(int i=0; i<sArray.length; i++)
+    //    {
+    //         pos =0;
+    //         pos1 =0;
+    //         for(int j=0;j<sArray.length && i!=j; j++)
+    //         {
+    //             if(sArray[i] == sArray[j])
+    //             {
+    //                  pos= pos+1;
+    //             }
+    //             if(tArray[i] == tArray[j])
+    //             {
+    //                  pos1= pos1+1;
+    //             }
+
+    //         }
+    //     shm.put(sArray[i], pos);
+    //     thm.put(tArray[i], pos1);
+
+
+    //    }
+
+        //optimising
+       
+       for(int i=0; i<tArray.length; i++)
+       {
+        if(thm.containsKey(tArray[i])) thm.put(tArray[i] , thm.get(tArray[i])+1 ) ;
+
+         else    thm.put(tArray[i], 1);
+        
+       }
+       for(int i=0; i<sArray.length; i++)
+       {
+        if(shm.containsKey(sArray[i])) shm.put(sArray[i] , shm.get(sArray[i])+1 ) ;
+         else    shm.put(sArray[i], 1);
+        
+       }
+
+       if(shm.equals(thm)) return true;
+       else return false;
+
+
+
+
+
+
+    }
+}
